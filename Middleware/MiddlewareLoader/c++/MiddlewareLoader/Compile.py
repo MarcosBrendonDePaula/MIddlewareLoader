ba5506029cd4c0d9838c407aad7d3674aeb08e24
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2021-12-06 13:27:11 -0300

    Exemplos de Console chats adicionados

--- Middleware/MiddlewareLoader/c++/MiddlewareLoader/Compile.py
+++ Middleware/MiddlewareLoader/c++/MiddlewareLoader/Compile.py
@@ -1,4 +1,4 @@
-import os
+import os,sys
 # Fuctions
 def getAll(path="",exp=".cpp"):
     files = []
@@ -36,24 +36,61 @@ cpp_files = getAll(include_path,".cpp")
 cpp_src_Server = getAll(src_path+"/Server",".cpp")
 cpp_src_Client = getAll(src_path+"/Client",".cpp")
 
-print("Compilando Servidor")
-os.system("{comp} {flags} {includes} {src} -o Server -l {libraries} -I {include_path}".format(
-    comp = Compiller,
-    flags = ListToStr(flags),  
-    includes = ListToStr(cpp_files),
-    src = ListToStr(cpp_src_Server),
-    out = output,
-    libraries = ListToStr(link_libraries),
-    include_path = include_path
-))
+if len(sys.argv) < 2:
+    print("-server      Compilar Servidor")
+    print("-client      Compilar Cliente")
+    print("-all         Compilar Servidor e cliente")
+else:
+    for param in sys.argv:
+        if(param.upper() == "-Server".upper()):
+            print("Compilando Servidor")
+            os.system("{comp} {flags} {includes} {src} -o Server -l {libraries} -I {include_path}".format(
+                comp = Compiller,
+                flags = ListToStr(flags),  
+                includes = ListToStr(cpp_files),
+                src = ListToStr(cpp_src_Server),
+                out = output,
+                libraries = ListToStr(link_libraries),
+                include_path = include_path
+            ))
+            print("Servidor Pronto")
+        elif(param.upper() == "-Client".upper()) :
+            print("Compilando Client")
+            os.system("{comp} {flags} {includes} {src} -o Client -l {libraries} -I {include_path}".format(
+                comp = Compiller,  
+                flags = ListToStr(flags),
+                includes = ListToStr(cpp_files),
+                src = ListToStr(cpp_src_Client),
+                out = output,
+                libraries = ListToStr(link_libraries),
+                include_path = include_path
+            ))
+            print("Cliente Pronto")
+        elif(param.upper() == "-all".upper()):
+            print("Compilando Servidor")
+            os.system("{comp} {flags} {includes} {src} -o Server -l {libraries} -I {include_path}".format(
+                comp = Compiller,
+                flags = ListToStr(flags),  
+                includes = ListToStr(cpp_files),
+                src = ListToStr(cpp_src_Server),
+                out = output,
+                libraries = ListToStr(link_libraries),
+                include_path = include_path
+            ))
+            print("Servidor Pronto")
+            print("Compilando Client")
+            os.system("{comp} {flags} {includes} {src} -o Client -l {libraries} -I {include_path}".format(
+                comp = Compiller,  
+                flags = ListToStr(flags),
+                includes = ListToStr(cpp_files),
+                src = ListToStr(cpp_src_Client),
+                out = output,
+                libraries = ListToStr(link_libraries),
+                include_path = include_path
+            ))
+            print("Cliente Pronto")
+    pass
 
-print("Compilando Client")
-os.system("{comp} {flags} {includes} {src} -o Client -l {libraries} -I {include_path}".format(
-    comp = Compiller,  
-    flags = ListToStr(flags),
-    includes = ListToStr(cpp_files),
-    src = ListToStr(cpp_src_Client),
-    out = output,
-    libraries = ListToStr(link_libraries),
-    include_path = include_path
-))
+
+
+
